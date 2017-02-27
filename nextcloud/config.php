195fab1624de6b97899a4a319eeed5d9388e3c60
root <root@cloudsrv001.unbelievable-machine.net>
2017-02-27 13:52:28 +0100

    added files

---
+++ nextcloud/config.php
@@ -0,0 +1,7 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocifrt5u2sjm',
+  'overwritehost'     => 'willi-net.ddns.net',
+  'overwriteprotocol' => 'https',
+  'overwritewebroot'  => '/nextcloud',
+);
